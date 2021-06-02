18d4c7e9b2bede1657ee67a0395c583447b63dcf
Getham1234 <73149122+Getham1234@users.noreply.github.com>
2021-06-01 20:00:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality: 90
+})
+cammera = document.getElementById("cammera");
+Webcam.attach('#cammera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+
+console.log("ml5 version", ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/[...]', modelLoaded);
+function modelLoaded(){
+    console.log("model loaded");
+}
